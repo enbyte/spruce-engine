7715a281d1dbcebc8a511da282aeaaf269f833ac
enbyte <sb-mangobrains@crowder-sklar.com>
2021-10-20 18:38:49 -0400

    fixed drops system and recipes

--- src/drops.py
+++ src/drops.py
@@ -1,5 +1,6 @@
 import json
 import random
+import inventory as inv
 
 def do_roll(percent):
   ''' Returns True percent % of the time and False (100 - percent) % of the time. '''
@@ -14,17 +15,17 @@ def do_roll(percent):
   return False
   
 def prettify(thing):
-    return thing.replace('_', ' ').title()
+    return thing.lstrip('item').replace('_', ' ').title().strip()
     
 
 class ItemDrops:
-  def __init__(self, json_file, is_file=True):
+  def __init__(self, json_file, is_file=True, name=None):
     '''
     Load an item-drop calc from a json file. 
+    Get the section "name" from the file. If name is none, use the entire file.
     File format: (note: @'s are for multiple drops, stripped at the end.)
     {
-      "name": "drops_blackthorn",
-      "drops": {
+      "drops_blackthorn": {
         "item_blackthorn": 100,
         "item_blackthorn@": 50,
         "item_blackthorn@@": 25,
@@ -37,8 +38,10 @@ class ItemDrops:
       f = json.load(open(json_file))
     else:
       f = json.loads(json_file)
-    self.name = f['name']
-    drops = f['drops']
+    if name == None:
+      drops = f
+    else:
+      drops = f[name]
     self.drops = {}
     self.item_name_keys = []
     for item_name_k in drops:
@@ -55,30 +58,45 @@ class ItemDrops:
     return drops
   
 if __name__ == '__main__':
-    blackthorn_drops = ItemDrops('''{
-      "name": "drops_blackthorn",
-      "drops": {
+  drops_str = '''{
+      "drops_blackthorn": {
         "item_blackthorn": 100,
         "item_blackthorn@": 50,
         "item_blackthorn@@": 25,
         "item_impure_thorn_essence": 2,
         "item_sharp_thorn": 10
-       }
-     }''', is_file = False)
-    piggin_drops = ItemDrops('''{
-      "name": "drops_piggin",
-      "drops": {
+       }, 
+       "drops_piggin": {
           "item_piggin_meat": 100,
           "item_piggin_meat@": 30,
           "item_piggin_entrails": 10,
           "item_piggin_skin": 100,
           "item_piggin_skin@": 30,
           "item_piggin_pet": 0.1
-        }
       }
-    ''', is_file=False)
-    for i in range(1000):
-      d = piggin_drops.get_drops()
-      if 'item_piggin_pet' in d:
-          print(f"It took {i + 1} tries to get a Piggin pet!")
-      
+    }
+    '''
+  blackthorn_drops = ItemDrops(drops_str, is_file=False, name='drops_blackthorn')
+  piggin_drops = ItemDrops(drops_str, is_file=False, name='drops_piggin')
+  player_inv = inv.Inventory()
+  while True:
+    response = ''
+    while not response in ('piggin', 'blackthorn', 'inventory'):
+      response = input("Enter piggin, blackthorn or inventory: ")
+    
+    is_drops = response in ('piggin', 'blackthorn')
+    if is_drops:
+      if response == 'piggin':
+        d = piggin_drops
+      elif response == 'blackthorn':
+        d = blackthorn_drops
+      pgdrps = d.get_drops()
+      cds = []
+      for i in pgdrps:
+        cds.append(prettify(i))
+        player_inv.add_item(inv.Item(i))
+      printable = ', '.join(cds)
+      print(f"You got {printable}!")
+    else:
+      if response == 'inventory':
+        player_inv.print_self()
