3d8ec7cd063eac2aa114f3956b58f66b71644741
enbyte <sb-mangobrains@crowder-sklar.com>
2021-12-24 10:28:02 -0500

    added useless demo

---
+++ src/demo.py
@@ -0,0 +1,15 @@
+import pygame
+
+from . import tilemap
+from . import ui
+from . import drops
+from . import inventory
+from . import rotate
+from . import object_manager
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
